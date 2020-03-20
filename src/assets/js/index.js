69bed4ea9632f28fd502954c9dd10e441fe6ae28
Aleksey <aleksey.kalyuznhyi@gmail.com>
2020-03-20 21:55:02 +0300

    added video and toggle videos

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,30 @@
-console.log(0 == [])
+
+const iframe = document.querySelector('.about-video iframe')
+const videoWrap = document.querySelector('.about-videos')
+const videoHome = document.querySelector('.about-video')
+videoHome.addEventListener('click', function() {
+        if(this.classList.contains('poster')) {
+            this.classList.remove('poster')
+            return
+        }
+})
+videoWrap.addEventListener('click', function(e) {
+    if(!e.target.closest('img')) return
+
+    if(videoHome.classList.contains('poster')) {
+        videoHome.classList.remove('poster')
+    }
+    let iframeSrc = iframe.getAttribute('src')
+    let videoSrc = e.target.getAttribute('data-videoSrc')
+
+    for(let i = 0; i < this.children.length; i++) {
+        if(this.children[i].classList.contains('video-wrap--active')){
+            this.children[i].classList.remove('video-wrap--active')
+            break;
+        }
+    }
+    let url = iframeSrc.replace(/[a-zA-Z0-9-.]*$/, videoSrc)
+    iframe.setAttribute('src', url)
+    e.target.classList.add('video-wrap--active')
+
+})
